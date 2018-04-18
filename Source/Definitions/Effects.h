6a84c48db87518fd4ddc3d04ede92d252056d818
LambertSchulze <38498379+LambertSchulze@users.noreply.github.com>
2018-04-18 19:08:24 +0200

    Update Effects.h

--- Source/Definitions/Effects.h
+++ Source/Definitions/Effects.h
@@ -56,3 +56,19 @@ private:
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NoEffect)
 };
+
+//==============================================================================
+class LeftSolo : public Effect
+{
+public:
+	LeftSolo ();
+	~LeftSolo ();
+	
+	void processEffect (const AudioSourceChannelInfo& bufferToFill);
+
+private:
+	String type {"Solo"};
+    String name {"LeftSolo"};
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LeftSolo)
+};
