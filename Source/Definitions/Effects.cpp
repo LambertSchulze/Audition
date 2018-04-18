40b0ffcf7632fc640cfa19a4224a5ff4ff11b525
LambertSchulze <38498379+LambertSchulze@users.noreply.github.com>
2018-04-18 19:14:19 +0200

    Update Effects.cpp

--- Source/Definitions/Effects.cpp
+++ Source/Definitions/Effects.cpp
@@ -57,3 +57,23 @@ void RightSolo::processEffect (const AudioSourceChannelInfo& bufferToFill)
 		leftChannel[sample] = 0;
 	}
 }
+
+//==============================================================================
+Mono::Mono ()	{reduction = Decibels::decibelsToGain(-2);}
+Mono::~Mono () 	{}
+
+void Mono::processEffect (const AudioSourceChannelInfo& bufferToFill)
+{
+	float* const leftChannel 	= bufferToFill.buffer->getWritePointer (0, bufferToFill.startSample);
+	float* const rightChannel 	= bufferToFill.buffer->getWritePointer (1, bufferToFill.startSample);
+
+	for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+	{
+		const float f 			= leftChannel[sample];
+		leftChannel[sample] 	+= rightChannel[sample];
+		rightChannel[sample] 	+= f;
+        
+        leftChannel[sample]     = leftChannel[sample] * reduction;
+        rightChannel[sample]    = rightChannel[sample] * reduction;
+	}
+}
