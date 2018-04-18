1243fd2dc301ccead2e7b1db5ddefc7e730473e5
LambertSchulze <38498379+LambertSchulze@users.noreply.github.com>
2018-04-18 19:11:02 +0200

    Update Effects.cpp

--- Source/Definitions/Effects.cpp
+++ Source/Definitions/Effects.cpp
@@ -13,19 +13,19 @@
 
 //==============================================================================
 Effect::Effect () {}
-
 Effect::~Effect() {}
 
-void Effect::process        (const AudioSourceChannelInfo& bufferToFill)
-{
-    Effect::processEffect   (bufferToFill);
-}
-
+void Effect::process        (const AudioSourceChannelInfo& bufferToFill) {Effect::processEffect   (bufferToFill);}
 void Effect::processEffect  (const AudioSourceChannelInfo& bufferToFill) {}
 
 void Effect::setName        (String name)   { this->name = name; }
 String Effect::getName      ()              { return this->name; }
 String Effect::getType      ()              { return type; }
-
 void Effect::setLevel       (float level)   { this->level = level; }
 float Effect::getLevel      ()              { return level; }
+
+//==============================================================================
+NoEffect::NoEffect ()  {}
+NoEffect::~NoEffect () {}
+
+void NoEffect::processEffect (const AudioSourceChannelInfo& bufferToFill) {}
