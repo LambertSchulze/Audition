5b29890328c8aff27a3b15f696e7ac2a8eebf5a4
LambertSchulze <38498379+LambertSchulze@users.noreply.github.com>
2018-04-18 19:16:05 +0200

    Update Effects.cpp

--- Source/Definitions/Effects.cpp
+++ Source/Definitions/Effects.cpp
@@ -77,3 +77,22 @@ void Mono::processEffect (const AudioSourceChannelInfo& bufferToFill)
         rightChannel[sample]    = rightChannel[sample] * reduction;
 	}
 }
+
+//==============================================================================
+SumVolume::SumVolume  () {reduction = Decibels::decibelsToGain(-2);}
+SumVolume::~SumVolume () {}
+
+void SumVolume::processEffect (const AudioSourceChannelInfo& bufferToFill)
+{
+	for (int channel = 0; channel < 2; ++channel)
+	{
+		float* const buffer = bufferToFill.buffer->getWritePointer (channel, bufferToFill.startSample);
+
+		for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+		{
+			buffer[sample] = buffer[sample] * level;
+		}
+	}
+}
+
+//==============================================================================
