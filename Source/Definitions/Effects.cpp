15e9529f6c53dd5a1edb88940c5fab7d0d984dbe
LambertSchulze <38498379+LambertSchulze@users.noreply.github.com>
2018-04-18 19:05:03 +0200

    Update and rename Effect.cpp to Effects.cpp

--- Source/Definitions/Effects.cpp
+++ Source/Definitions/Effects.cpp
@@ -9,7 +9,7 @@
  */
 
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "Effect.h"
+#include "Effects.h"
 
 //==============================================================================
 Effect::Effect () {}
