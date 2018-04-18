ba956d5a711f6381101449b5f164403df64d43a6
LambertSchulze <38498379+LambertSchulze@users.noreply.github.com>
2018-04-18 19:06:24 +0200

    Update Effects.h

--- Source/Definitions/Effects.h
+++ Source/Definitions/Effects.h
@@ -40,3 +40,19 @@ protected:
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Effect)
 };
+
+//==============================================================================
+class NoEffect : public Effect
+{
+public:
+	NoEffect ();
+	~NoEffect ();
+
+	void processEffect (const AudioSourceChannelInfo& bufferToFill) override;
+
+private:
+	String type {"Other"};
+    String name {"NoEffect"};
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NoEffect)
+};
