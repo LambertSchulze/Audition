5e9d82d42e012a0c3401ae5bf8831ab0e5161c57
Lambert Schulze <38498379+LambertSchulze@users.noreply.github.com>
2018-05-14 19:42:44 +0200

    Update TransportComponentLookAndFeel.h

--- Source/Gui/TransportComponentLookAndFeel.h
+++ Source/Gui/TransportComponentLookAndFeel.h
@@ -1,1 +1,27 @@
+/*
+ ==============================================================================
+ 
+ TransportComponentLookAndFeel.h
+ Created: 14 May 2018 8:51:56pm
+ Author:  Lambert Schulze
+ 
+ ==============================================================================
+ */
 
+#pragma once
+
+#include "../../JuceLibraryCode/JuceHeader.h"
+#include "AuditionLookAndFeel.h"
+
+class TransportComponentLookAndFeel :    public LookAndFeel_V4
+{
+public:
+    TransportComponentLookAndFeel ()
+    {
+        
+    };
+    
+    ~TransportComponentLookAndFeel () {};
+    
+    AuditionLookAndFeel laf;
+};
