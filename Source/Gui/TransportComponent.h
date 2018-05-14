8e805a32dded72cbe5903552dcf5626a4d268e49
Lambert Schulze <38498379+LambertSchulze@users.noreply.github.com>
2018-05-14 19:41:00 +0200

    Update TransportComponent.h

--- Source/Gui/TransportComponent.h
+++ Source/Gui/TransportComponent.h
@@ -5,3 +5,37 @@
     Author:  Lambert Schulze
   ==============================================================================
 */
+
+#include "../JuceHeader.h"
+#include "TransportComponentLookAndFeel.h"
+
+class TransportComponent :  Component,
+                            ValueTree::Listener
+{
+public:
+    TransportComponent (ValueTree);
+    
+    ˜TransportComponent();
+    
+    void paint (Graphics);
+    void resized ();
+    
+    void valueTreePropertyChanged (ValueTree&, const Identifier&);
+    void valueTreeChildAdded (ValueTree&, ValueTree&);
+    void valueTreeChildRemoved (ValueTree&, ValueTree&, int);
+    void valueTreeChildOrderChanged (ValueTree&, int, int);
+    void valueTreeParentChanged (ValueTree&);
+    void valueTreeRedirected (ValueTree&);
+    
+    void originalButtonClicked();
+    void effectButtonClicked();
+    
+public:
+    ValueTree tree;
+    TransportComponentLookAndFeel lookAndFeel;
+
+    DrawableButton  OriginalButton, EffectButton;
+    Label           InfoLabel;
+    
+    
+}
